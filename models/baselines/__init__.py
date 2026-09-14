f0df7e6404e95d78f24824f3582d64f9967d5149
zcy8998 <zhangchenyu2024@bupt.edu.cn>
2026-09-14 12:23:54 +0800

    Remove unavailable baseline export

--- models/baselines/__init__.py
+++ models/baselines/__init__.py
@@ -1,12 +1,9 @@
 from .csinet_adapter import CSINetAdapter
 from .deepmimo_feedback import CsiNetLSTMAdapter
 from .transnet_adapter import TransNetAdapter
-from .wifo_adapter import WiFoBeamAdapter, WiFoFeedbackAdapter
 
 __all__ = [
     "CSINetAdapter",
     "CsiNetLSTMAdapter",
     "TransNetAdapter",
-    "WiFoBeamAdapter",
-    "WiFoFeedbackAdapter",
 ]
